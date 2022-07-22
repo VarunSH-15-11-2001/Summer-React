f7e62cefe8cda6b90f2b130778b3d9655a9536f9
VarunSH-15-11-2001 <varunhoskere@gmail.com>
2022-07-22 15:14:39 +0530

    test

--- react-complete-guide/src/components/Chart/Chart.js
+++ react-complete-guide/src/components/Chart/Chart.js
@@ -3,7 +3,7 @@ import React from "react";
 import ChartBar from "./ChartBar";
 import "./Chart.css";
 
-const Chart = (props) => {
+const Chart = props => {
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
